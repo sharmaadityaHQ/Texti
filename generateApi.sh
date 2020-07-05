d1afe523bc3857102f63d94e66fee38b780d50dd
Aditya Sharma <sharmaaditya570191@gmail.com>
2020-07-05 17:53:28 +0530

    Add documentation plugin

--- generateApi.sh
+++ generateApi.sh
@@ -1,6 +1,9 @@
 echo "God is processing your request..."
+sleep 5
 output=$(node readText.js)
 echo "Your API is being generated..."
+sleep 5
 cd app/
 npm run strapi -- generate:api $output
 npm run develop
+firefox http://localhost:1337/documentation/v1.0.0#/
