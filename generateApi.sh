0b8261ba09b90a1fd7dd057e3226179bfd0c8e4a
Aditya Sharma <sharmaaditya570191@gmail.com>
2020-07-05 20:06:41 +0530

    Change sleep time

--- generateApi.sh
+++ generateApi.sh
@@ -1,8 +1,8 @@
 echo "God is processing your request..."
-sleep 5
+sleep 3
 output=$(node readText.js)
 echo "Your API is being generated..."
-sleep 5
+sleep 3
 cd app/
 npm run strapi -- generate:api $output
 npm run develop
